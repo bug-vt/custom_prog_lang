bb34258586e5b89dcec3f7df169673297c8edfc0
Bug Lee <bug19@vt.edu>
2022-09-15 12:57:23 -0400

    Added assembler by integrating parser and code generator. Added decoder for translating binary to ASCII.

---
+++ src/assembler/assembler.cpp
@@ -0,0 +1,71 @@
+/*
+ * This implement assembler, which generate byte code for custom VM.
+ * It takes assembly code that was adapted from the Alex Varanese's
+ * XtremeScript assembly.
+ * The output executable is formatted as follow:
+ * 1. Header
+ * 2. Instruction stream
+ * 3. String table
+ * 4. Function table
+ */
+
+#include <fstream>
+#include <sstream>
+#include "asm_parser.hpp"
+#include "error.hpp"
+
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::stringstream;
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+
+string loadAssemblyFile (char *file_name);
+
+
+int main (int argc, char **argv)
+{
+  if (argc < 2)
+    exitOnError ("usage: assembler assembly_file <output_file_name>");
+ 
+  string out_file_name = "out.executable";
+  if (argc > 2)
+    out_file_name = string (argv[2]) + ".executable";
+
+  string raw_source = loadAssemblyFile (argv[1]);
+  AsmParser parser (raw_source);
+  parser.parse ();
+
+  ofstream output_file (out_file_name);
+  CodeGen code_gen = parser.createCodeGen (output_file);
+  code_gen.setVersion (VERSION_MAJOR, VERSION_MINOR);   
+  code_gen.writeHeader ();
+  code_gen.writeInstrStream ();
+  code_gen.writeStringTable ();
+  code_gen.writeFuncTable ();
+
+  output_file.close ();
+
+  return 0;
+}
+
+string loadAssemblyFile (char *file_name)
+{
+  // open the input file
+  ifstream input_file (file_name);
+  
+  if (input_file.fail ())
+    exitOnError ("fail to open given input file: " + string (file_name));
+
+  // load assembly code into memory
+  stringstream buffer;
+  buffer << input_file.rdbuf ();
+
+  // close the file
+  input_file.close ();
+
+  return buffer.str ();
+}
+
---
+++ src/assembler/decoder.cpp
@@ -0,0 +1,189 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include "instruction.hpp"
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ofstream;
+using std::ifstream;
+using std::stringstream;
+
+
+string readHeader (ifstream &binary);
+string readInstrStream (ifstream &binary);
+string readStringTable (ifstream &binary);
+string readFuncTable (ifstream &binary);
+
+
+struct TestHeader
+{
+  char ver_major;
+  char ver_minor;
+  int stack_size;
+  int global_data_size;
+  char is_main_func_present;
+  int main_func_index;
+};
+
+struct TestOp
+{
+    OpType type;
+    union
+    {
+        int int_literal;
+        float float_literal;
+        int str_table_index;    // string table index
+        int instr_index;        // instruction stream index
+        int stack_index;        // stack index
+        int func_index;         // function table index
+        int reg;                // register code
+    };
+    int offset_index;           // stack index of the offset variable
+};
+
+
+int main (int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    cout << "usage: decoder executable_file" << endl;
+    exit (1);
+  }
+
+  string out = "";
+  ifstream binary (argv[1]);
+
+  out += readHeader (binary) + "\n";
+  out += readInstrStream (binary) + "\n";
+  out += readStringTable (binary) + "\n";
+  out += readFuncTable (binary) + "\n";
+
+  binary.close ();
+
+  cout << out;
+
+  return 0;
+}
+
+
+string readHeader (ifstream &binary)
+{
+  // read header
+  TestHeader header;
+  binary.read ((char *) &header, sizeof (TestHeader));
+
+  stringstream out;
+  out << "Header:" << endl
+      << (int) header.ver_major << " "
+      << (int) header.ver_minor << endl
+      << header.stack_size << " "
+      << header.global_data_size << " "
+      << (bool) header.is_main_func_present << " "
+      << header.main_func_index << endl;
+
+  return out.str ();
+}
+
+string readInstrStream (ifstream &binary)
+{
+  stringstream out;
+  // read size of instruction stream
+  int size;
+  binary.read ((char *) &size, sizeof (int));
+
+  out << "Instruction stream:" << endl
+      << size << endl;
+
+  // read each instruction
+  for (int i = 0; i < size; i++)
+  {
+    // read opcode and operand count
+    char opcode, op_count;
+    binary.read (&opcode, sizeof (char));
+    binary.read (&op_count, sizeof (char));
+   
+    out << (int) opcode << " "
+        << (int) op_count;
+    if (op_count > 0)
+      out << " ";
+    else
+      out << endl;
+
+    // read list of operands that are used in this instruction
+    for (int op_index = 0; op_index < op_count; op_index++)
+    {
+      TestOp op;
+      binary.read ((char *) &op, sizeof (TestOp));
+
+      out << op.type << " "
+          << op.int_literal << " "
+          << op.offset_index;
+      
+      if (op_index < op_count - 1)
+        out << " ";
+      else
+        out << endl;
+    }
+  }
+
+  return out.str ();
+}
+
+string readStringTable (ifstream &binary)
+{
+  stringstream out;
+  // read size of string table 
+  int size;
+  binary.read ((char *) &size, sizeof (int));
+
+  out << "String table:" << endl
+      << size << endl;
+
+  // read each string
+  for (int i = 0; i < size; i++)
+  {
+    // read string length 
+    int len;
+    binary.read ((char *) &len, sizeof (int));
+   
+    char str[len + 1]; 
+    str[len] = '\0';
+    binary.read (str, len);
+
+    out << len << " "
+        << string (str) << endl;
+  }
+
+  return out.str ();
+}
+
+string readFuncTable (ifstream &binary)
+{
+  stringstream out;
+  // read size of function table 
+  int size;
+  binary.read ((char *) &size, sizeof (int));
+
+  out << "Function table:" << endl
+      << size << endl;
+
+  // read each function 
+  for (int i = 0; i < size; i++)
+  {
+    int entry_point;
+    int param_count;
+    int local_data_size;
+    binary.read ((char *) &entry_point, sizeof (int));
+    binary.read ((char *) &param_count, sizeof (int));
+    binary.read ((char *) &local_data_size, sizeof (int));
+
+    out << entry_point << " "
+        << param_count << " "
+        << local_data_size << endl;
+  }
+
+  return out.str ();
+}
